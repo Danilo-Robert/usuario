1bf89ba16a01bf1a810dfbb29bf0a4a0ad488303
Danilo <99danilorobert@gmail.com>
2025-11-10 17:57:16 -0300

    "Refatoração do método de login e autenticação de usuários"

--- src/main/java/com/danilo/usuario/infrastructure/security/JwtRequestFilter.java
+++ src/main/java/com/danilo/usuario/infrastructure/security/JwtRequestFilter.java
@@ -1,9 +1,16 @@
 package com.danilo.usuario.infrastructure.security;
 
+import com.danilo.usuario.infrastructure.exceptions.dto.ErrorResponseDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +18,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class JwtRequestFilter extends OncePerRequestFilter {
 
@@ -25,6 +33,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
+        try {
 
         final String authorizationHeader = request.getHeader("Authorization");
 
@@ -42,5 +51,32 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             }
         }
         chain.doFilter(request, response);
+    } catch (ExpiredJwtException e){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write(buildError(HttpStatus.UNAUTHORIZED.value(),
+                    "Token expirado",
+                    request.getRequestURI(),
+                    e.getMessage()));
+        }
+    }
+
+    private String buildError(int status, String mensagem, String path, String error){
+        ErrorResponseDTO errorResponseDTO = ErrorResponseDTO.builder()
+                .timestamp(LocalDateTime.now())
+                .message(mensagem)
+                .error(error)
+                .status(status)
+                .path(path)
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        try {
+            return objectMapper.writeValueAsString(errorResponseDTO);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
